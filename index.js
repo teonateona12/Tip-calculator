302485b3514cc69f667256870d7e70c921ff4a27
teona piranishvili <teona.piranishvili1@gmail.com>
2022-10-11 18:12:32 +0400

    fixing bugs

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
 "strict mode";
-//select buttons
-const btnOne = document.querySelector(".btn-one");
-const btnTwo = document.querySelector(".btn-two");
-const btnThree = document.querySelector(".btn-three");
-const btnFour = document.querySelector(".btn-four");
-const btnFive = document.querySelector(".btn-five");
+
+//select elements
 const btnSix = document.querySelector(".btn-six");
+const btns = document.querySelectorAll(".btn");
 
 const billInp = document.querySelector(".bill-inp");
 const peopleInp = document.querySelector(".people-inp");
 const tipAmount = document.querySelector(".tip-number");
 const totalNumber = document.querySelector(".total-number");
 const reset = document.querySelector(".reset");
+const error = document.querySelector(".error");
 
-// button addeventlisteners
-btnOne.addEventListener("click", (e) => {
+
+
+//only positive number of people
+peopleInp.addEventListener("input", (e) => {
   e.preventDefault();
-  tipAndTotal(5);
-});
-btnTwo.addEventListener("click", (e) => {
-  e.preventDefault();
-  tipAndTotal(10);
-});
-btnThree.addEventListener("click", (e) => {
-  e.preventDefault();
-  tipAndTotal(15);
-});
-btnFour.addEventListener("click", (e) => {
-  e.preventDefault();
-  tipAndTotal(25);
-});
-btnFive.addEventListener("click", (e) => {
-  e.preventDefault();
-  tipAndTotal(50);
+  if (peopleInp.value < 0) {
+    peopleInp.value = "";
+  }
 });
 
-// btnSix.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   tipAndTotal(btnSix.textContent);
-// });
+//only positive number of bill
+billInp.addEventListener("input", (e) => {
+  e.preventDefault();
+  if (billInp.value < 0) {
+    billInp.value = "";
+  }
+});
+
+//buttons click
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    tipAndTotal(+btns[i].value);
+  });
+}
+
+//custom
+btnSix.addEventListener("input", (e) => {
+  e.preventDefault();
+  tipAndTotal(btnSix.value);
+  btnSix.classList.add("border");
+});
 
 //reset
 reset.addEventListener("click", () => {
@@ -46,13 +50,31 @@ reset.addEventListener("click", () => {
   peopleInp.value = "";
   totalNumber.textContent = "$0.00";
   tipAmount.textContent = "$0.00";
+  btnSix.value = "";
+  error.textContent = "";
+  peopleInp.classList.remove("error-inp");
 });
 
-//
+//calculate tip and total
 const tipAndTotal = (persent) => {
-  let devide = (billInp.value / 100) * persent;
-  let result = devide / peopleInp.value;
-  tipAmount.textContent = `$${Math.round(result)}`;
-  let total = billInp.value / peopleInp.value + result;
-  totalNumber.textContent = `$${Math.round(total)}`;
+  if (+peopleInp.value === 0) {
+    error.textContent = "Can't be zero";
+    peopleInp.classList.add("error-inp");
+    totalNumber.textContent = "$0.00";
+    tipAmount.textContent = "$0.00";
+  } else {
+    let devide = (billInp.value / 100) * persent;
+    let result = devide / peopleInp.value;
+    tipAmount.textContent = `$${result.toFixed(2)}`;
+    let total = billInp.value / peopleInp.value + result;
+    totalNumber.textContent = `$${total.toFixed(2)}`;
+    error.textContent = "";
+    peopleInp.classList.remove("error-inp");
+  }
+
+  //if number is empty
+  if (peopleInp.value === "") {
+    error.textContent = "";
+    peopleInp.classList.remove("error-inp");
+  }
 };
